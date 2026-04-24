0d80a46df891deda0e9e218017dd9c4b6c0eeb2a
Lior Shwarzwald <lior@Liors-MacBook-Pro.local>
2026-04-24 11:29:51 +0300

    edit form: full edit+delete on all tabs (hotels, flights, activities, todos)

--- api/save.js
+++ api/save.js
@@ -31,21 +31,51 @@ export default async function handler(req, res) {
   // Apply the action
   if (action === 'addHotel') {
     current.HOTELS.push(data);
+  } else if (action === 'editHotel') {
+    const { index, ...hotelData } = data;
+    if (index < 0 || index >= current.HOTELS.length) return res.status(400).json({ error: 'Invalid hotel index' });
+    current.HOTELS[index] = hotelData;
   } else if (action === 'deleteHotel') {
     current.HOTELS.splice(data.index, 1);
+
   } else if (action === 'addFlight') {
     current.FLIGHTS.push(data);
     current.FLIGHTS.sort((a, b) => a.date.localeCompare(b.date));
+  } else if (action === 'editFlight') {
+    const { index, ...flightData } = data;
+    if (index < 0 || index >= current.FLIGHTS.length) return res.status(400).json({ error: 'Invalid flight index' });
+    current.FLIGHTS[index] = flightData;
+    current.FLIGHTS.sort((a, b) => a.date.localeCompare(b.date));
+  } else if (action === 'deleteFlight') {
+    current.FLIGHTS.splice(data.index, 1);
+
   } else if (action === 'addActivity') {
     current.ACTIVITIES.push(data);
     current.ACTIVITIES.sort((a, b) => a.date.localeCompare(b.date));
+  } else if (action === 'editActivity') {
+    const { index, ...actData } = data;
+    if (index < 0 || index >= current.ACTIVITIES.length) return res.status(400).json({ error: 'Invalid activity index' });
+    current.ACTIVITIES[index] = actData;
+    current.ACTIVITIES.sort((a, b) => a.date.localeCompare(b.date));
   } else if (action === 'deleteActivity') {
     current.ACTIVITIES.splice(data.index, 1);
+
   } else if (action === 'toggleTodo') {
     const todo = current.TODOS.find(t => t.id === data.id);
     if (todo) todo.done = data.done;
   } else if (action === 'addTodo') {
     current.TODOS.push(data);
+  } else if (action === 'editTodo') {
+    const todo = current.TODOS.find(t => t.id === data.id);
+    if (todo) {
+      todo.label   = data.label;
+      todo.labelHe = data.labelHe;
+      todo.notes   = data.notes;
+      todo.notesHe = data.notesHe;
+    }
+  } else if (action === 'deleteTodo') {
+    const idx = current.TODOS.findIndex(t => t.id === data.id);
+    if (idx !== -1) current.TODOS.splice(idx, 1);
   } else {
     return res.status(400).json({ error: 'Unknown action' });
   }
